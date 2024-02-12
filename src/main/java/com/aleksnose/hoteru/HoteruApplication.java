429d29df9735e2ec3bc46b2d6b609fd252969d85
Aleksandra Nosińska <aleks.nosinska@gmail.com>
2024-02-12 14:54:51 +0100

    Configuration project

---
+++ src/main/java/com/aleksnose/hoteru/HoteruApplication.java
@@ -0,0 +1,13 @@
+package com.aleksnose.hoteru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoteruApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoteruApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aleksnose/hoteru/HoteruApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aleksnose.hoteru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoteruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
