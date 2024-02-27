fb3a939be74fd6c353632490ee7ff379afb44b7d
Aleksandra Nosińska <aleks.nosinska@gmail.com>
2024-02-27 23:59:12 +0100

    Remove model CountryRepository

--- src/main/java/com/aleksnose/hoteru/repository/CountryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.aleksnose.hoteru.repository;
-
-import com.aleksnose.hoteru.models.Country;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CountryRepository extends JpaRepository<Country, Integer> {
-}
